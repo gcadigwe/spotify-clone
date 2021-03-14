8c4c0178ffd4bb1aa0f700533674b581b15c950f
GCAdigwe <gcadigwe@gmail.com>
2021-03-14 20:44:19 +0000

    initialize firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDERM93h5F_Nd0Tr5FewH0l_ZISHZb-pGQ",
+  authDomain: "spotify-clone-aca93.firebaseapp.com",
+  projectId: "spotify-clone-aca93",
+  storageBucket: "spotify-clone-aca93.appspot.com",
+  messagingSenderId: "433819151585",
+  appId: "1:433819151585:web:7846d8980d575997b51ba0",
+  measurementId: "G-JZZ6QMXQY3",
+};
+
+const app = firebase.initializeApp(firebaseConfig);
