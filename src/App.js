d2c2c98ba9c2192d2773f4546a7ffed7f0902b70
GCAdigwe <gcadigwe@gmail.com>
2021-03-15 15:17:03 +0000

    done with sidebar components

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import Login from "./components/Login/Login";
 import { getTokenFromUrl } from "./spotify";
 import Player from "./components/Player/Player";
 import SpotifyWebApi from "spotify-web-api-js";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
   const dispatch = useDispatch();
+  const { state } = useSelector((state) => ({ ...state }));
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -18,6 +19,10 @@ function App() {
 
     if (_token) {
       setToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        payload: _token,
+      });
 
       spotify.setAccessToken(_token);
 
@@ -28,11 +33,23 @@ function App() {
           payload: user,
         });
       });
+
+      spotify.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLIST",
+          payload: playlists,
+        });
+      });
     }
 
     window.location.hash = "";
   }, []);
-  return <div className="app">{token ? <Player /> : <Login />}</div>;
+  console.log("TOKEN from redux >>", state.token);
+  return (
+    <div className="app">
+      {token ? <Player spotify={spotify} /> : <Login />}
+    </div>
+  );
 }
 
 export default App;
--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,8 @@ const initialState = {
   playlists: [],
   playing: false,
   item: null,
+  token:
+    "BQAkynQ-WDRg9v1OTahLlKYdln-4GJxhs2tv-JmPiRbWFrtEDQFjIb4fGYc5FtE8vYc5hbXZEaa6gvJjpDwESX717NABcnNexqfESq5engLtIe8Vfl-NBb3iXQ2eG-x5U9IsHkZ4Cn1fDqastn6m0OH5GZrgjea_kJdNd8dLqu0A3tl6",
 };
 
 const Reducer = (state = initialState, action) => {
@@ -11,6 +13,18 @@ const Reducer = (state = initialState, action) => {
       return {
         ...state,
         user: action.payload,
+      };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.payload,
+      };
+
+    case "SET_PLAYLIST":
+      return {
+        ...state,
+        playlists: action.payload,
       };
     default:
       return state;
